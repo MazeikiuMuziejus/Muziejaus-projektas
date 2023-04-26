4d692f9cedfff1a75b988b4195c9389230ee30fc
Smikis <redasdonkus@gmail.com>
2023-04-26 16:34:22 +0300

    cleanup

--- index.js
+++ index.js
@@ -3,16 +3,5 @@ import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import React from 'react';
-
-import GestureHandlerRootView from 'react-native-gesture-handler';
-
-const ProvidedApp = () => {
-  return (
-    <GestureHandlerRootView style={{flex: 1}}>
-      <App />
-    </GestureHandlerRootView>
-  );
-};
 
 AppRegistry.registerComponent(appName, () => App);
